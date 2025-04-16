a431a22555f8e21e4eac3c1ed1b4e974b927df4a
Jose Diaz <josepabloadiazjofre@gmail.com>
2025-04-16 17:51:38 -0400

    retoques finales, comentarios extra explicando algunas partes del codigo, simplificacion de estilos css eliminando decoraciones que no ocupe (pero use en uno de los laboratorios)

--- CER1-DIAZJOSE/js/carrusel.js
+++ CER1-DIAZJOSE/js/carrusel.js
@@ -1,6 +1,6 @@
-const images = document.querySelectorAll('.carousel-image');
+const images = document.querySelectorAll('.carousel-image'); //busca las imagenes que tengan esa clase
 const prevBtn = document.querySelector('.carousel-btn.prev');
-const nextBtn = document.querySelector('.carousel-btn.next');
+const nextBtn = document.querySelector('.carousel-btn.next');//se buscan los botones que tengan esa clase para poder usar el addEventListener
 let currentIndex = 0;
 
 function showImage(index) {
@@ -9,13 +9,13 @@ function showImage(index) {
     });
 }
 
-prevBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
+prevBtn.addEventListener('click', () => { //addEventListener funciona al hacer click
+    currentIndex = (currentIndex - 1 + images.length) % images.length; //calculo el cual permite que si se esta en el indice 0, y apretamos el boton, el nuevo indice sea la ultima imagen del array images
     showImage(currentIndex);
 });
 
 nextBtn.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length;
+    currentIndex = (currentIndex + 1) % images.length; //mismo calculo pero para el boton siguiente, si estamos en la ultima imagen y apretamos el boton, el nuevo indice sera 0 pues usamos el modulo
     showImage(currentIndex);
 });
 
